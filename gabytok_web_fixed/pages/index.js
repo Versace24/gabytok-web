0e2a01e57adc609f127be89e97600515d5af7d53
Versace24 <49383706+Versace24@users.noreply.github.com>
2025-04-28 22:56:04 -0400

    Add files via upload

---
+++ gabytok_web_fixed/pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Bienvenido a Gabytok Web 🎥🚀</h1>
+      <p>Tu TikTok personalizado ya está activo.</p>
+    </div>
+  );
+}
